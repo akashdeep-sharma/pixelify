59314056d7207a0511dfbe418367479b50e10535
Akashdeep Sharma <akashdeepsharma@Akashdeeps-MacBook-Pro.local>
2023-01-03 23:47:33 +0530

    added exceptions in delete pixel getAllmetafields

--- web/helper.js
+++ web/helper.js
@@ -1,52 +1,51 @@
-
-
-import { GraphqlQueryError } from "@shopify/shopify-api";
-import shopify from "./shopify.js";
-import getRawBody from "raw-body";
-
-
+import { GraphqlQueryError } from '@shopify/shopify-api';
+import shopify from './shopify.js';
+import getRawBody from 'raw-body';
 
 import fs from 'fs';
 
+export async function getAllMetfieldsValues(data) {
+  var session = data.res.locals.shopify.session;
+  try {
+    const clientRes = new shopify.api.clients.Rest({ session });
+    const response = await clientRes.get({
+      path: 'metafields',
+      query: { key: 'pixelify', namespace: 'IDs' },
+    });
 
-
-export async function getAllMetfieldsValues(data){
-    var session = data.res.locals.shopify.session;
-    const clientRes = new shopify.api.clients.Rest({session})
-    const response = await clientRes.get({path: 'metafields', 	query: { "key": "pixelify",
-    "namespace": "IDs" }});
-    
     return response.body.metafields[0].value;
+  } catch (e) {
+    console.log(`${e} session info :{${sessions}}`);
+  }
 }
 
-
-
-export async function deletPixelID(data){
-  var  newID = await getRawBody(data._req);
+export async function deletPixelID(data) {
+  var newID = await getRawBody(data._req);
   newID = newID.toString();
   newID = JSON.parse(newID);
-  console.log("here",newID.id);
+  console.log('here', newID.id);
 
   var session = data.res.locals.shopify.session;
-  const clientRes = new shopify.api.clients.Rest({session})
-  const response = await clientRes.get({path: 'metafields', 	query: { "key": "pixelify",
-  "namespace": "IDs" }});
-  
-  var allpixelID = JSON.parse(response.body.metafields[0].value);
+  try {
+    const clientRes = new shopify.api.clients.Rest({ session });
+    const response = await clientRes.get({
+      path: 'metafields',
+      query: { key: 'pixelify', namespace: 'IDs' },
+    });
 
-  console.log(allpixelID);
+    var allpixelID = JSON.parse(response.body.metafields[0].value);
 
-  var removedpixelIDs = allpixelID.filter(function(ele){ 
-    return ele != newID.id; 
-});
+    console.log(allpixelID);
 
+    var removedpixelIDs = allpixelID.filter(function (ele) {
+      return ele != newID.id;
+    });
 
-console.log(removedpixelIDs);
-   
-const client = new shopify.api.clients.Graphql({ session });
+    console.log(removedpixelIDs);
 
+    const client = new shopify.api.clients.Graphql({ session });
 
-const queryString = `{
+    const queryString = `{
   shop {
     id
     name
@@ -55,107 +54,97 @@ const queryString = `{
     taxesIncluded
   }
 
-}`
+}`;
 
+    const shop = await client.query({
+      data: queryString,
+    });
 
-const shop = await client.query({
-  data: queryString,
-});
+    var newlyMeta = await client.query({
+      data: {
+        query: CREATE_METFIELD_MUTATION,
+        variables: {
+          metafields: [
+            {
+              key: 'pixelify',
+              namespace: 'IDs',
+              type: 'list.number_integer',
+              value: JSON.stringify(removedpixelIDs),
+              ownerId: shop.body.data.shop.id,
+            },
+          ],
+        },
+      },
+    });
 
-  var newlyMeta = await client.query({
+    console.log(JSON.stringify(newlyMeta.body.data.metafieldsSet));
+
+    return;
+  } catch (e) {
+    console.log(`${e} session-info {${session}}`);
+  }
+}
+
+async function addScriptIntoTheme(data) {
+  var session = data.session;
+  const clientRes = new shopify.api.clients.Rest({ session });
+
+  const response = await clientRes.get({
+    path: 'themes',
+    query: { role: 'main' },
+  });
+
+  console.log(response.body.themes[0]);
+
+  // get existing theme.liquid code
+  const response2 = await clientRes.get({
+    path: `themes/${response.body.themes[0].id}/assets`,
+    query: {
+      asset: { key: 'layout/theme.liquid' },
+    },
+  });
+
+  // console.log(response2.body.asset.value);
+
+  var themeCode = response2.body.asset.value;
+
+  var updatedThemeCode = themeCode.replace(
+    '<head>',
+    "<head>{% render 'MultiPixelPixelify' %}"
+  );
+
+  // console.log(updatedThemeCode);
+
+  const response3 = await clientRes.put({
+    // update theme.liquid
+    path: `themes/${response.body.themes[0].id}/assets`,
     data: {
-      query: CREATE_METFIELD_MUTATION,
-      variables: {
-
-            "metafields": [
-              {
-                "key": "pixelify",
-                "namespace": "IDs",
-                "type": "list.number_integer",
-                "value": JSON.stringify(removedpixelIDs),
-                "ownerId": shop.body.data.shop.id,
-              }
-            ]
-          
+      asset: {
+        key: 'layout/theme.liquid',
+        value: updatedThemeCode,
       },
     },
   });
-  
-  console.log( JSON.stringify(newlyMeta.body.data.metafieldsSet) );
 
+  // console.log("final",response3.body);
+
+  const { pixelTemplate } = JSON.parse(fs.readFileSync('./config.json')); // get pixel code from local file
+
+  const response4 = await clientRes.put({
+    // add new snippet with pixel code
+    path: `themes/${response.body.themes[0].id}/assets`,
+    data: {
+      asset: {
+        key: 'snippets/MultiPixelPixelify.liquid',
+        value: pixelTemplate,
+      },
+    },
+  });
+
+  console.log('done', response4.body);
 
   return;
 }
-
-
-async function addScriptIntoTheme(data){
-    var session = data.session;
-    const clientRes = new shopify.api.clients.Rest({session})
-
-
-
-    const response = await clientRes.get({path: 'themes', 	query: { role: 'main'}});
-
-    console.log(response.body.themes[0]);
-
-    // get existing theme.liquid code
-    const response2 = await clientRes.get({path: `themes/${response.body.themes[0].id}/assets`, 	query: {   
-        asset: {"key": "layout/theme.liquid"}
-        }
-    });
-
-
-    // console.log(response2.body.asset.value);
-
-    var themeCode = response2.body.asset.value;
-
-    
-
-    var updatedThemeCode = themeCode.replace('<head>',"<head>{% render 'MultiPixelPixelify' %}");
-
-    // console.log(updatedThemeCode);
-
-    const response3 = await clientRes.put({ // update theme.liquid
-        path: `themes/${response.body.themes[0].id}/assets`,
-        data: {
-            asset: {
-                "key": "layout/theme.liquid",
-                "value": updatedThemeCode
-            }
-          },
-      });
-
-     // console.log("final",response3.body);
-
-     const {pixelTemplate} = JSON.parse(fs.readFileSync('./config.json')); // get pixel code from local file
-
-     const response4 = await clientRes.put({ // add new snippet with pixel code
-        path: `themes/${response.body.themes[0].id}/assets`,
-        data: {
-            asset: {
-                "key": "snippets/MultiPixelPixelify.liquid",
-                "value": pixelTemplate
-            }
-          },
-      });
-
-      console.log("done",response4.body);
-
-
-
-
-
-
-
-
-
-
-
-
-    return;
-}
-
-
 
 const CREATE_METFIELD_MUTATION = `
 mutation MetafieldsSet($metafields: [MetafieldsSetInput!]!) {
@@ -176,22 +165,12 @@ mutation MetafieldsSet($metafields: [MetafieldsSetInput!]!) {
   }
 `;
 
-
-
-
-
-
-
 export async function addMetafield(data) {
+  var session = data.res.locals.shopify.session;
 
+  const client = new shopify.api.clients.Graphql({ session });
 
-
-    var session = data.res.locals.shopify.session;
-
-
-    const client = new shopify.api.clients.Graphql({ session });
-    
-    const queryString = `{
+  const queryString = `{
             shop {
               id
               name
@@ -200,86 +179,67 @@ export async function addMetafield(data) {
               taxesIncluded
             }
 
-      }`
+      }`;
 
+  const shop = await client.query({
+    data: queryString,
+  });
 
-    const shop = await client.query({
-        data: queryString,
-      });
+  console.log(JSON.stringify(shop.body.data.shop));
 
-      console.log(JSON.stringify(shop.body.data.shop))
+  const clientRes = new shopify.api.clients.Rest({ session });
+  const response = await clientRes.get({
+    path: 'metafields',
+    query: { key: 'pixelify', namespace: 'IDs' },
+  });
 
+  console.log('allMetafileds', response.body.metafields);
 
+  var newID = await getRawBody(data._req);
 
+  newID = newID.toString();
+  newID = JSON.parse(newID);
 
+  var pixelIDs = [newID.id];
+  console.log('id', pixelIDs);
 
+  if (response.body.metafields.length == 0) {
+    addScriptIntoTheme({ session });
+  } else {
+    pixelIDs = pixelIDs.concat(JSON.parse(response.body.metafields[0].value)); // add new pixel ID with existing ID's
+  }
 
-    const clientRes = new shopify.api.clients.Rest({session})
-    const response = await clientRes.get({path: 'metafields', 	query: { "key": "pixelify",
-    "namespace": "IDs" }});
-    
+  console.log(pixelIDs);
 
-
-
-      console.log("allMetafileds", response.body.metafields);
-
-
-      var  newID = await getRawBody(data._req);
-      
-      newID = newID.toString();
-      newID = JSON.parse(newID);
-
-      var pixelIDs = [newID.id];
-      console.log('id', pixelIDs);
-
-      if(response.body.metafields.length == 0){
-        addScriptIntoTheme({session});
-      }
-      else{
-        pixelIDs = pixelIDs.concat(JSON.parse(response.body.metafields[0].value)); // add new pixel ID with existing ID's
-      }
-
-      console.log(pixelIDs); 
-
-    try {
-
-          var newlyMeta = await client.query({
-            data: {
-              query: CREATE_METFIELD_MUTATION,
-              variables: {
-
-                    "metafields": [
-                      {
-                        "key": "pixelify",
-                        "namespace": "IDs",
-                        "type": "list.number_integer",
-                        "value": JSON.stringify(pixelIDs),
-                        "ownerId": shop.body.data.shop.id,
-                      }
-                    ]
-                  
-              },
+  try {
+    var newlyMeta = await client.query({
+      data: {
+        query: CREATE_METFIELD_MUTATION,
+        variables: {
+          metafields: [
+            {
+              key: 'pixelify',
+              namespace: 'IDs',
+              type: 'list.number_integer',
+              value: JSON.stringify(pixelIDs),
+              ownerId: shop.body.data.shop.id,
             },
-          });
-          
-          console.log( JSON.stringify(newlyMeta.body.data.metafieldsSet) );
+          ],
+        },
+      },
+    });
 
-      } catch (error) {
-        console.log("error")
-        if (error instanceof GraphqlQueryError) {
-          throw new Error(
-            `${error.message}\n${JSON.stringify(error.response, null, 2)}`
-          );
-        } else {
-          throw error;
-        }
-      }
+    console.log(JSON.stringify(newlyMeta.body.data.metafieldsSet));
+  } catch (error) {
+    console.log('error');
+    if (error instanceof GraphqlQueryError) {
+      throw new Error(
+        `${error.message}\n${JSON.stringify(error.response, null, 2)}`
+      );
+    } else {
+      throw error;
+    }
+  }
 
-
-      
-
-    
-    return;
+  return;
 }
-
-
